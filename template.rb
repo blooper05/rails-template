9d24478fd32094eb3fc4581166153ceacfa91028
blooper05 <legend.of.blooper@gmail.com>
2016-05-19 08:01:49 +0900

    add settings for exception_notification

--- template.rb
+++ template.rb
@@ -209,3 +209,25 @@ create_file '.env'
 gsub_file 'config/environments/production.rb', /# config\.logger = .+/ do
   'config.logger = ::ChronoLogger.new("#{config.paths[:log][0]}.%Y%m%d")'
 end
+
+# exception_notification
+initializer 'exception_notification.rb', <<~'CODE'
+  require 'exception_notification/rails'
+
+  ExceptionNotification.configure do |config|
+    # do not notify in development env and test env
+    config.ignore_if do
+      Rails.env.development? || Rails.env.test?
+    end
+
+    # notification to Slack
+    config.add_notifier(
+      :slack, webhook_url: Settings.slack.webhook_url,
+              channel:     Settings.slack.channel,
+              username:    Settings.slack.username,
+              additional_parameters: { icon_emoji: Settings.slack.icon },
+              http_options: { proxy_address: Settings.proxy.address,
+                              proxy_port:    Settings.proxy.port }
+    )
+  end
+CODE
