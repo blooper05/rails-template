f9ff7c8cf2224da2b90c07c9eff77bc261c2f4fe
blooper05 <legend.of.blooper@gmail.com>
2016-05-21 21:43:50 +0900

    add settings for annotate

--- template.rb
+++ template.rb
@@ -397,3 +397,11 @@ insert_into_file 'spec/rails_helper.rb', <<-CODE, before: /^end$/
   config.before(:each) { Bullet.start_request }
   config.after(:each) { Bullet.end_request }
 CODE
+
+### annotate ###
+generate 'annotate:install'
+
+annotate_file = 'lib/tasks/auto_annotate_models.rake'
+gsub_file annotate_file, /'routes'\s+=>\s+'false'/ do |match|
+  match.sub('false', 'true')
+end
