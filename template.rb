6b783726c543a736f5574376adee23e0f467f6cd
blooper05 <legend.of.blooper@gmail.com>
2016-05-21 13:38:29 +0900

    add settings for pry-byebug

--- template.rb
+++ template.rb
@@ -320,6 +320,23 @@ append_file '.pryrc', <<~'CODE'
   end
 CODE
 
+### pry-byebug ###
+append_file '.pryrc', <<~CODE
+
+  ### pry-byebug ###
+  if defined?(PryByebug)
+    Pry.commands.alias_command 'c', 'continue'
+    Pry.commands.alias_command 's', 'step'
+    Pry.commands.alias_command 'n', 'next'
+    Pry.commands.alias_command 'f', 'finish'
+  end
+
+  # Hit Enter to repeat last command
+  Pry::Commands.command(/^$/, 'repeat last command') do
+    _pry_.run_command Pry.history.to_a.last
+  end
+CODE
+
 ### rspec-rails ###
 generate 'rspec:install'
 
