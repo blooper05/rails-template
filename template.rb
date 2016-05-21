b907d296479920b2d01524b7ec22b00d797f3495
blooper05 <legend.of.blooper@gmail.com>
2016-05-21 20:56:39 +0900

    add settings for bullet

--- template.rb
+++ template.rb
@@ -382,3 +382,19 @@ create_file '.simplecov', <<~CODE
 
   # vim: set ft=ruby:
 CODE
+
+### bullet ###
+insert_into_file 'config/environments/test.rb', <<-CODE, before: /^end$/
+
+  ### Bullet ###
+  Bullet.enable        = true
+  Bullet.bullet_logger = true
+  Bullet.raise         = true
+CODE
+
+insert_into_file 'spec/rails_helper.rb', <<-CODE, before: /^end$/
+
+  ### Bullet ###
+  config.before(:each) { Bullet.start_request }
+  config.after(:each) { Bullet.end_request }
+CODE
