7259a22ffe57ea11d24045f0030b17066f1803d3
blooper05 <legend.of.blooper@gmail.com>
2017-07-09 23:06:27 +0900

    :+1: Update settings for rubocop

--- templates/project.rb
+++ templates/project.rb
@@ -4,38 +4,7 @@
 run 'spring binstub --all'
 
 ### rubocop ###
-COPS = %w[
-  Lint/UnusedBlockArgument
-  Style/AndOr
-  Style/DefWithParentheses
-  Style/EmptyLineBetweenDefs
-  Style/EmptyLines
-  Style/EmptyLinesAroundAccessModifier
-  Style/EmptyLinesAroundBlockBody
-  Style/EmptyLinesAroundClassBody
-  Style/EmptyLinesAroundMethodBody
-  Style/EmptyLinesAroundModuleBody
-  Style/ExtraSpacing
-  Style/HashSyntax
-  Style/IndentationConsistency
-  Style/IndentationWidth
-  Style/MethodCallParentheses
-  Style/MethodDefParentheses
-  Style/MultilineOperationIndentation
-  Style/SpaceAfterColon
-  Style/SpaceAfterComma
-  Style/SpaceAfterNot
-  Style/SpaceAfterSemicolon
-  Style/SpaceAroundEqualsInParameterDefault
-  Style/SpaceAroundOperators
-  Style/SpaceBeforeSemicolon
-  Style/SpaceInsideHashLiteralBraces
-  Style/StringLiterals
-  Style/TrailingBlankLines
-  Style/TrailingWhitespace
-].freeze
-
-run "rubocop -a --only #{COPS.join(',')}"
+run 'rubocop --auto-correct'
 
 ### git init ###
 after_bundle do
