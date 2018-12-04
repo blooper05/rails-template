d83e1de0d3eed1b4a09df7dc603d78af670da808
blooper05 <legend.of.blooper@gmail.com>
2018-12-04 18:09:44 +0900

    :shower: Remove settings about git init

--- templates/project.rb
+++ templates/project.rb
@@ -5,16 +5,3 @@ run 'spring binstub --all'
 
 ### rubocop ###
 run 'rubocop --auto-correct'
-
-### git init ###
-after_bundle do
-  remove_file '.git'
-  remove_file 'template.rb'
-  remove_file 'templates'
-
-  git :init
-  git commit: "--allow-empty -m ':tada: Initial commit'"
-  git commit: <<~MSG
-    -a -m ':shell: rails new . -m template.rb -d postgresql -GCMSTB --api -f'
-  MSG
-end
